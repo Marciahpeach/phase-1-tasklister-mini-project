2738a5089000956838a527d57171011ff0a23619
Marciah <marciah.machoka@student.moringaschool.com>
2025-04-04 01:16:12 +0300

    completed changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+//your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const taskDescription = document.getElementById("new-task-description").value;
+    const taskList = document.getElementById("tasks");
+    
+    const newTask = document.createElement("li");
+    newTask.textContent = taskDescription;
+    taskList.appendChild(newTask);
+    
+    document.getElementById("new-task-description").value = "";
+  });
 });
